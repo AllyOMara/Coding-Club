563dfeb2bbc95bef7d1d56c407e7bc0f83a73586
Ally <124349321+AllyOMara@users.noreply.github.com>
2025-04-25 17:08:31 +1000

    Create Turtle_Star.py
    A little program that draws a star! The size of the star is based on the user's input.

---
+++ Turtle_Star.py
@@ -0,0 +1,31 @@
+# Importing the turtle
+from turtle import *
+from turtle import *
+
+# Chose the size of the sides of the star
+length = int(input('How long do you want the sides of the star to be? '))
+
+# Drawing the star
+pensize(8)
+pencolor('gold')
+forward(length)
+left(144)
+forward(length)
+right(72)
+forward(length)
+left(144)
+forward(length)
+right(72)
+forward(length)
+left(144)
+forward(length)
+right(72)
+forward(length)
+left(144)
+forward(length)
+right(72)
+forward(length)
+left(144)
+forward(length)
+
+# Enjoy your star!! :D
