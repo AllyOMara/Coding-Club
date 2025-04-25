4110ed40560fe4d90a3c546458f0f9d462df5fe1
Ally <124349321+AllyOMara@users.noreply.github.com>
2025-04-25 18:30:52 +1000

    Update Basketball_Bunny.py
    Added comments

--- Grok_Beginner_Python/Basketball_Bunny.py
+++ Grok_Beginner_Python/Basketball_Bunny.py
@@ -1,6 +1,7 @@
-
+# User inputs defines the number of dunks performed by the bunny
 number_of_dunks = int(input('How many slam dunks? '))
 
+# Determines whether the user's number of dunks beats the world record
 if number_of_dunks > 7 :
   print("New world record!")
 else:
