7689804d37a8eb260112473176c8d7bb018a2b7f
Ally <124349321+AllyOMara@users.noreply.github.com>
2025-04-25 18:25:05 +1000

    Create Spell_it_out_for_me.py
    Grok module 6... long name...

---
+++ Grok_Beginner_Python/Spell_it_out_for_me.py
@@ -0,0 +1,11 @@
+# User inputs the word to spell out
+Word = input("Word: ")
+
+print(Word)
+
+# In the grok module (module 6), we learnt how to use for letter in [variable]
+# We use it here to print out all the letters in the word inputted above on separate lines
+for letter in Word:
+  print(letter)
+
+print(Word)
