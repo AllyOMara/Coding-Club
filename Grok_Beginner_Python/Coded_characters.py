58d1ce94af22343fec414b48bc88bd14e1ce35ae
Ally <124349321+AllyOMara@users.noreply.github.com>
2025-04-25 18:55:09 +1000

    Create Coded_characters.py
    Changing between chr and ord - Unicode and ordinal

---
+++ Grok_Beginner_Python/Coded_characters.py
@@ -0,0 +1,8 @@
+# Defining the number from the user input, converting into an integer
+number = int(input('Number? '))
+
+# Turning the integer into the character it represents in Unicode
+letter = chr(number)
+
+# Prints the Unicode character
+print(letter)
