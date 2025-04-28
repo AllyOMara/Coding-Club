08c0ad1d65f0d95a2aec35ca4f8496ce1d08758d
Ally <124349321+AllyOMara@users.noreply.github.com>
2025-04-28 14:11:18 +1000

    Create Symbol_simulator.py

---
+++ Grok_Beginner_Python/Symbol_simulator.py
@@ -0,0 +1,7 @@
+# Defines the function make_symbols
+def make_symbols(symbol, number):
+  return(symbol * number)
+
+# Testing
+print(make_symbols('!', 5)) # Should output !!!!!
+print(make_symbols('?', 3)) # Should output ???
