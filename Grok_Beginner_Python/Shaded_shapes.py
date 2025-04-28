8525a8fca6efdc30677b4cc82acc8bcaeb366fb6
Ally <124349321+AllyOMara@users.noreply.github.com>
2025-04-28 12:47:45 +1000

    Create Shaded_shapes.py

---
+++ Grok_Beginner_Python/Shaded_shapes.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+# Colours setup
+bgcolor('skyblue')
+fillcolor('royalblue')
+pensize(7)
+pencolor('mediumblue')
+side_number = int(input("How many sides? "))
+
+# Finding out how much to turn after each side is drawn
+turn_angle = 360 / side_number
+
+begin_fill()
+
+# For loop to create the whole shape
+for i in range(side_number):
+  forward(45)
+  left(turn_angle)
+
+# Fills the shape with colour
+end_fill()
