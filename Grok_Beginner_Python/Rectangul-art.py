310f041ea563efdc0127c2018af6ddf38e71677a
Ally <124349321+AllyOMara@users.noreply.github.com>
2025-04-29 17:51:36 +1000

    Create Rectangul-art.py

---
+++ Grok_Beginner_Python/Rectangul-art.py
@@ -0,0 +1,24 @@
+from turtle import
+
+# Defining the function draw_rect to draw a rectangle
+def draw_rect(width, height, colour):
+  # Finish the function to draw a rectangle:
+  pensize(3)
+  colour = pencolour(colour)
+  forward(width)
+  left(90)
+  forward(height)
+  left(90)
+  forward(width)
+  left(90)
+  forward(height)
+  left(90)
+
+# Drawing the buildings
+draw_rect(30, 120, 'royalblue')
+goto(-10, 0)
+draw_rect(80, 30, 'cornflowerblue')
+goto(-30, 0)
+draw_rect(80, 60, 'coral')
+goto(-45, 0)
+draw_rect(50, 80, 'plum')
