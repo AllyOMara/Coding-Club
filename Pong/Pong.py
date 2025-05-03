6fffb36650311743452e44a01f407202ada47388
AllyOMara <allystellaomara@gmail.com>
2025-05-03 19:32:08 +1000

    Update Pong.py

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -5,7 +5,7 @@ pygame.init()
 # Colours
 BLACK = (0,  0,  0)
 WHITE = (255,255,255)
-red = (255, 0, 0)
+
 # Screen setup
 size = (800,500)
 screen = pygame.display.set_mode(size)
@@ -14,26 +14,25 @@ FPS = 20
 screen.fill(BLACK)
 pygame.display.set_caption("Pong")
 
+def draw_player_1(width, height, x, y):
+    pygame.draw.rect(screen, WHITE, (width, height, x, y))
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y = y + 100
+                pygame.display.update()
+            if event.key == pygame.K_DOWN:
+                y = y - 100
+                pygame.display.update()
 
 # Game loop
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
-        print(event)
-
+        draw_player_1(30,200,20,125)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
-                quit()
-    
-    pygame.draw.rect(screen, WHITE, (30,200,20,125))
-    pygame.display.update()
 
     pygame.draw.rect(screen, WHITE, (750,200,20,125))
     pygame.display.update()
-
-
-
-
-#Shutdown
-pygame.quit()
