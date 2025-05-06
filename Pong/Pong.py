0f078057d18151d5ea4743fa67d42c7dd09456ca
AllyOMara <allystellaomara@gmail.com>
2025-05-06 20:27:34 +1000

    Update Pong.py --> Add variable player_move_units to replace values

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -18,6 +18,7 @@ running = True
 # Player positions
 player_1_x = 20
 player_1_y = 150
+player_move_units = 50
 
 # Game loop
 while running:
@@ -26,9 +27,9 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             if event.key == pygame.K_UP:
-                player_1_y = player_1_y - 50
+                player_1_y = player_1_y - player_move_units
             if event.key == pygame.K_DOWN:
-                player_1_y = player_1_y + 50
+                player_1_y = player_1_y + player_move_units
         elif event.type == pygame.QUIT:
             running = False
     screen.fill(BLACK)
